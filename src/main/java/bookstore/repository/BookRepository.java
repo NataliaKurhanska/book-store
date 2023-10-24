947c0a939bdf0fd8111229417d5c29f7d5f58309
Natalia Kurhanska <kurhnatali@gmail.com>
2023-10-24 14:17:21 +0300

    created Book, BookRepository, BookService classes

---
+++ src/main/java/bookstore/repository/BookRepository.java
@@ -0,0 +1,8 @@
+package bookstore.repository;
+
+import bookstore.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+}
