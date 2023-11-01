cbe91a5f50635ed88117665ac007de0984b67c76
Natalia Kurhanska <kurhnatali@gmail.com>
2023-11-01 12:28:16 +0200

    added Specifications

--- src/main/java/bookstore/repository/BookRepository.java
+++ src/main/java/bookstore/repository/BookRepository.java
@@ -3,7 +3,9 @@ package bookstore.repository;
 import bookstore.model.Book;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long>,
+        JpaSpecificationExecutor<Book> {
     Optional<Book> findBookById(Long id);
 }
--- src/main/java/bookstore/service/impl/BookServiceImpl.java
+++ src/main/java/bookstore/service/impl/BookServiceImpl.java
@@ -1,15 +1,18 @@
 package bookstore.service.impl;
 
 import bookstore.dto.BookDto;
+import bookstore.dto.BookSearchParameters;
 import bookstore.dto.CreateBookRequestDto;
 import bookstore.exception.EntityNotFoundException;
 import bookstore.mapper.BookMapper;
 import bookstore.model.Book;
 import bookstore.repository.BookRepository;
+import bookstore.repository.book.BookSpecificationBuilder;
 import bookstore.service.BookService;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -17,6 +20,7 @@ import org.springframework.stereotype.Service;
 public class BookServiceImpl implements BookService {
     private final BookRepository bookRepository;
     private final BookMapper bookMapper;
+    private final BookSpecificationBuilder specificationBuilder;
 
     @Override
     public BookDto save(CreateBookRequestDto bookRequestDto) {
@@ -52,5 +56,13 @@ public class BookServiceImpl implements BookService {
     @Override
     public void deleteById(Long id) {
         bookRepository.deleteById(id);
+    }
+
+    @Override
+    public List<BookDto> search(BookSearchParameters bookSearchParameters) {
+        Specification<Book> build = specificationBuilder.build(bookSearchParameters);
+        return bookRepository.findAll(build).stream()
+                .map(bookMapper::toDto)
+                .collect(Collectors.toList());
     }
 }
